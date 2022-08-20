23d06f6f3d66a4e8cb4a8776841c26db790e3247
Fernando May <fmay130@gmail.com>
2022-08-20 17:15:09 -0500

    Initial Commit

---
+++ Shared/sweatershopApp.swift
@@ -0,0 +1,17 @@
+//
+//  sweatershopApp.swift
+//  Shared
+//
+//  Created by Fernando May on 20/08/22.
+//
+
+import SwiftUI
+
+@main
+struct sweatershopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
